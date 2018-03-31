f2e237c44585ab6d60ac3ee8571fc9a18cf8186d
desingerjeoung <iili1010@naver.com>
2018-03-31 16:28:28 +0900

    2차 업로드

---
+++ js/progressBar.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.tabs-menu li:nth-child(3)').click(function(){
+        $('.photoshop').animate({width:'80%'},1400),
+        $('.illustration').animate({width:'80%'},1400),
+        $('.sketch').animate({width:'70%'},1400),
+        $('.xd').animate({width:'70%'},1400),
+        $('.premiere').animate({width:'60%'},1400),
+        $('.aftereffect').animate({width:'50%'},1400),
+        $('.indesign').animate({width:'60%'},1400),
+        $('.lightroom').animate({width:'50%'},1400),
+        $('.html').animate({width:'60%'},1400),
+        $('.css').animate({width:'60%'},1400)
+    })
+    
+});
